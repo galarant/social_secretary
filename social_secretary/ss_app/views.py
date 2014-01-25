ac516486684e2c09fbb5a138c8d9d2523c3d9cfc
Jeff <jeff.revesz@buzzfeed.com>
2014-01-24 23:54:59 -0500

    added basic views and templates for fb_connect and set_contacts

--- social_secretary/ss_app/views.py
+++ social_secretary/ss_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def fb_connect(request):
+    return render(request, 'fb_connect.html')
+
+def set_contacts(request):
+    return render(request, 'set_contacts.html')
--- social_secretary/urls.py
+++ social_secretary/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from social_secretary.ss_app.views import (
+    fb_connect,
+    set_contacts,
+)
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +14,6 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'fb_connect', fb_connect),
+                       url(r'set_contacts', set_contacts),
                        )
