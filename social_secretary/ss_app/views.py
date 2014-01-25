bac91ec9249bc94bceb458f0088b645998efc350
Jeff <jeff.revesz@buzzfeed.com>
2014-01-25 04:09:05 -0500

    removed comments in views

--- social_secretary/ss_app/views.py
+++ social_secretary/ss_app/views.py
@@ -9,10 +9,6 @@ from social_secretary.settings import (
 		FACEBOOK_APP_SECRET as app_secret,
 )
 
-
-# Create your views here.
-
-
 def fb_connect(request):
     return render(request, 'fb_connect.html')
 
@@ -21,14 +17,9 @@ def set_contacts(request):
     return render(request, 'set_contacts.html')
 
 def fb_login_callback(request):
-		#pete: this pretty-prints the whole dictionary including
-		#the query string (if there is one)
 		creds = request.META['QUERY_STRING'].split('&')
 		fbtoken = creds[0].split('=')[1]
 		fb_usrid = creds[1].split('=')[1]
 		graph = GraphAPI(fbtoken)
 		utils.get_extended_access_token(fbtoken, app_id, app_secret)
-		#parse exchange and store in extended user model
-		#userfacebookinfo: a new class that extends user from auth.
-		#fields: id (big integer field) and oauthtoken
 		return render(request, 'fb_login_request.html')
