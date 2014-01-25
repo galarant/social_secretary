d814c6a5ccc5e1700eacb53f410e3c2fc546f313
Jeff <jeff.revesz@buzzfeed.com>
2014-01-25 04:09:14 -0500

    pep8

--- social_secretary/ss_app/views.py
+++ social_secretary/ss_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from facepy import ( 
-	  GraphAPI,
-	  utils,
+from facepy import (
+    GraphAPI,
+    utils,
 )
 
 from social_secretary.settings import (
-    FACEBOOK_APP_ID as app_id, 
-		FACEBOOK_APP_SECRET as app_secret,
+    FACEBOOK_APP_ID as app_id,
+    FACEBOOK_APP_SECRET as app_secret,
 )
+
 
 def fb_connect(request):
     return render(request, 'fb_connect.html')
@@ -16,10 +17,11 @@ def fb_connect(request):
 def set_contacts(request):
     return render(request, 'set_contacts.html')
 
+
 def fb_login_callback(request):
-		creds = request.META['QUERY_STRING'].split('&')
-		fbtoken = creds[0].split('=')[1]
-		fb_usrid = creds[1].split('=')[1]
-		graph = GraphAPI(fbtoken)
-		utils.get_extended_access_token(fbtoken, app_id, app_secret)
-		return render(request, 'fb_login_request.html')
+    creds = request.META['QUERY_STRING'].split('&')
+    fbtoken = creds[0].split('=')[1]
+    fb_usrid = creds[1].split('=')[1]
+    graph = GraphAPI(fbtoken)
+    utils.get_extended_access_token(fbtoken, app_id, app_secret)
+    return render(request, 'fb_login_request.html')
