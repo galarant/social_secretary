8cf07da38f6c426402149ee6cc29efa65eaacab6
Junsong Li <ljs.darkfish@gmail.com>
2014-01-25 03:01:30 -0500

    direct user to signin; add custom signin form

--- social_secretary/ss_app/views.py
+++ social_secretary/ss_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def fb_connect(request):
     return render(request, 'fb_connect.html')
 
 
 def set_contacts(request):
     return render(request, 'set_contacts.html')
+
--- social_secretary/urls.py
+++ social_secretary/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 from social_secretary.ss_app.views import (
     fb_connect,
     set_contacts,
 )
-import userena
 
 admin.autodiscover()
 
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'fb_connect', fb_connect),
                        url(r'set_contacts', set_contacts),
-                       url(r'', 'userena.views.signin'),
+                       url(r'^/?$', RedirectView.as_view(url='/accounts/signin')),
+                       url(r'^accounts/signin/?$', 'userena.views.signin', {'template_name' : 'signin.html'}),
                        url(r'^accounts/', include('userena.urls')),
                        )
